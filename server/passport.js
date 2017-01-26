c52539e9eac7f144c5b3ff9dd7b1731ff11ce98c
Lukas von Mateffy <lukas.mateffy@gmail.com>
2017-01-26 21:39:03 +0100

    Fixed auth

--- server/passport.js
+++ server/passport.js
@@ -17,7 +17,7 @@ module.exports = passport => {
     {
       consumerKey: process.env.TWITTER_CONSUMER_KEY,
       consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      callbackURL: 'http://localhost:5000/auth/twitter/callback'
+      callbackURL: process.env.TWITTER_CALLBACK
     },
     function(token, tokenSecret, profile, done) {
       // make the code asynchronous
